576d0816451f0c695493c613e9db883d6500ed70
brai <leminhtri27204@gmail.com>
2026-04-04 14:48:13 +0700

    Hoàn thành chức năng thanh toán

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import TicketBooking from './TicketBooking.jsx';
+import CheckoutPayment from './CheckoutPayment.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/dat-ve" element={<TicketBooking />} />
+      <Route path="/thanh-toan" element={<CheckoutPayment />} />
     </Routes>
   );
 }
