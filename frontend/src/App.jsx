8214dfc68598e6507e84647dd8759307ffa7ce2f
quanghung9504 <nguyenquanghungdepzai1@gmail.com>
2026-04-04 16:09:49 +0700

    Hoan thanh dem nguoc giu ve va thong bao su kien

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import TicketBooking from './TicketBooking.jsx';
+import Announcements from './Announcements.jsx';
+import PaymentStep from './PaymentStep.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/thong-bao" element={<Announcements />} />
       <Route path="/dat-ve" element={<TicketBooking />} />
+      <Route path="/thanh-toan" element={<PaymentStep />} />
     </Routes>
   );
 }
